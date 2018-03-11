6de0f25629611981b3936c79e6c2ad4588f9b01d
parhelia <crufty@gmail.com>
2018-03-11 12:23:21 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by ET on 2018-03-11.
+//  Copyright © 2018 ET. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
